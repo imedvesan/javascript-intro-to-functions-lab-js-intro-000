90e6f10bb523f77a5f4aa7240d02d1e1d7e86bde
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-01-27 20:17:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ var uppercase = "HELLO";
 function sayHiToGrandma(string) {
   if(lowercase.toLowerCase() === lowercase) {
     return "I can\'t hear you!";
-  } else if (uppercase.toUpperCase() === uppercase) {
+  }
+  if (uppercase.toUpperCase() === uppercase) {
     return "YES INDEED!";
-  } else if (string === "I love you, Grandma!") {
+  }
+  if (string === "I love you, Grandma!") {
     return "I love you, too!";
   }
 }
