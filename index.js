efa88afb99a2e1c4c5fc0701b219e1209ee63d13
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-01-27 19:57:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string.toLowerCase() === lowercase) {
-    returns "I can\'t hear you!";
+    return "I can\'t hear you!";
+  } else if (string.toUpperCase() === uppercase) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma!") {
+    return "I love you, too!";
   }
 }
